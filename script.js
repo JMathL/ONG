af11fe2eafb01b094a401983ffdb92be10a9b2e5
Matheus <thetheus.lim@gmail.com>
2025-08-05 15:26:01 -0300

    Update

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, node!")
+     function backToTop() {
+       window.scrollTo({
+         top: 0,
+         behavior: 'smooth'
+       });
+     }
+
+     window.onscroll = function() {
+       const backToTopButton = document.getElementById("voltar_ao_topo");
+       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         backToTopButton.classList.add("show");
+       } else {
+         backToTopButton.classList.remove("show");
+       }
+     };
