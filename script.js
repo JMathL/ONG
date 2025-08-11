fac850c55d5ce4608544da7b39469a7c731b9ed6
Matheus <thetheus.lim@gmail.com>
2025-08-11 13:24:47 -0300

    Update

--- script.js
+++ script.js
@@ -13,3 +13,28 @@
          backToTopButton.classList.remove("show");
        }
      };
+
+    
+    const botaoOn = document.getElementById("botao_enviar")
+    
+     botaoOn.addEventListener("click",  function() {
+      alert("Botão clicado!")
+    })
+     
+    document.addEventListener('DOMContentLoaded', function() {
+    const faqQuestions = document.querySelectorAll('.faq_pergunta');
+
+    faqQuestions.forEach(question => {
+        question.addEventListener('click', function() {
+            const answer = this.nextElementSibling;
+            
+            answer.classList.toggle('show');
+
+            faqQuestions.forEach(q => {
+                if (q !== this) {
+                    q.nextElementSibling.classList.remove('show');
+                }
+            });
+        });
+    });
+});
